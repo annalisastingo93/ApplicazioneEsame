262e64693a588c89c449c76d4cdc450dfadb2cb7
matteocaiazzo <matteo.caiazzo@libero.it>
2019-05-22 11:38:11 +0200

    matteo was here

--- app/src/main/java/com/example/applicazioneesame/MainActivity.java
+++ app/src/main/java/com/example/applicazioneesame/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-// Porfirio was here
+// Matteo was here
 public class MainActivity extends AppCompatActivity {
 
 
